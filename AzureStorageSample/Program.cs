f48cccef439e05476feb445f0d3a365eab9892f9
Brant Jones <wijonesi@microsoft.com>
2018-04-18 09:54:23 -0700

    Initial code commit

--- AzureStorageSample/Program.cs
+++ AzureStorageSample/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
 
 namespace AzureStorageSample
 {
@@ -11,5 +18,98 @@ namespace AzureStorageSample
         static void Main(string[] args)
         {
         }
+
+        private class AzureConfiguration
+        {
+            public static string StorageContainerName = "myazureblobcontainer";
+            public static string StorageAcccountName = "portalvhdswxs6mj9gfl2sq";
+            public static string StorageAccountKey = "RzxsY0hk24ikQrIpphFgVdEtc7zGk0eH89npEowoZObwVDBQt3DR+gwYgeKLkvDCFiPHcrORix6C5SJ97WoEUA==";
+            public static string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + StorageAcccountName + ";AccountKey=" + StorageAccountKey;
+            
+
+            public static CloudStorageAccount Account = CloudStorageAccount.Parse(StorageConnectionString);
+        }
+
+        #region " Azure Blob Storage "
+        static void getBlobs(ref CloudStorageAccount account, string containerName)
+        {
+            var blobClient = account.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(containerName);
+
+            foreach (var item in container.ListBlobs(null, false))
+            {
+                if (item.GetType() == typeof(Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob))
+                {
+                    var blob = (Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob)item;
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        blob.DownloadToStream(stream);
+
+                        Console.WriteLine("Blob name: {1}\tContent: {0}", System.Text.Encoding.UTF8.GetString(stream.ToArray()), blob.Name);
+                    }
+                }
+            }
+        }
+        static string getBlob(ref CloudStorageAccount account, string containerName, string blobName)
+        {
+            var blobResults = string.Empty;
+
+            try
+            {
+                var blobClient = account.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(containerName);
+                var blockBlob = container.GetBlockBlobReference(blobName);
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    blockBlob.DownloadToStream(stream);
+
+                    using (StreamReader sr = new StreamReader(stream, System.Text.Encoding.GetEncoding(1252)))
+                    {
+                        blobResults = sr.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ignore)
+            {
+                // return empty string - zero length string denotes download failed or invalid Block Blob
+            }
+
+            return blobResults;
+        }
+
+        static void storeBlobs(ref CloudStorageAccount acct, string containerName)
+        {
+            var files = Directory.GetFiles(@"C:\Projects\azureFiles\");
+
+            foreach (var file in files)
+            {
+                var blobName = file.Substring(file.LastIndexOf("\\") + 1, file.Length - file.LastIndexOf("\\") - 5);
+
+                var fileData = new MemoryStream(File.ReadAllBytes(file));
+
+                Console.WriteLine(blobName + " | " + storeBlob(ref acct, containerName, blobName, fileData).ToString());
+            }
+        }
+
+        static bool storeBlob(ref CloudStorageAccount account, string containerName, string blobName, MemoryStream stream)
+        {
+            try
+            {
+                var blobClient = account.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(containerName);
+                var blockBlob = container.GetBlockBlobReference(blobName);
+                blockBlob.UploadFromStream(stream);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
     }
 }
